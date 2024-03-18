a8e466d1786348cae19d30f073a0a044c859e578
Vincenque <DudaWitold.DW@gmail.com>
2024-03-18 16:57:55 +0100

    FFT being calculated but something is wrong.

--- lib/synchronize_signals_impl.cc
+++ lib/synchronize_signals_impl.cc
@@ -7,12 +7,41 @@
 
 #include "synchronize_signals_impl.h"
 #include <gnuradio/io_signature.h>
+#include <complex>
+#include <vector>
+#include <cmath>
 
 namespace gr {
 namespace synchronize_signals_module {
 
 using input_type = gr_complex;
 using output_type = gr_complex;
+
+const double PI = 3.14159265358979323846;
+
+void fft(std::vector<std::complex<double>>& x) {
+    const size_t N = x.size();
+    if (N <= 1) return;
+
+    // divide
+    std::vector<std::complex<double>> even(N / 2);
+    std::vector<std::complex<double>> odd(N / 2);
+    for (size_t i = 0; i < N / 2; ++i) {
+        even[i] = x[i * 2];
+        odd[i] = x[i * 2 + 1];
+    }
+
+    // conquer
+    fft(even);
+    fft(odd);
+
+    // combine
+    for (size_t i = 0; i < N / 2; ++i) {
+        std::complex<double> t = std::polar(1.0, -2 * PI * i / N) * odd[i];
+        x[i] = even[i] + t;
+        x[i + N / 2] = even[i] - t;
+    }
+}
 
 synchronize_signals::sptr synchronize_signals::make(size_t itemsize, bool synchronize)
 {
@@ -45,6 +74,36 @@ int synchronize_signals_impl::work(int noutput_items,
         const input_type* in1 = static_cast<const input_type*>(input_items[0]);
         const input_type* in2 = static_cast<const input_type*>(input_items[1]);
 
+        // Perform FFT on both inputs
+        std::vector<std::complex<double>> fft1(in1, in1 + noutput_items);
+        std::vector<std::complex<double>> fft2(in2, in2 + noutput_items);
+        fft(fft1);
+        fft(fft2);
+
+        // Calculate the conjugate of the second FFT and multiply it with the first FFT
+        for (size_t i = 0; i < static_cast<size_t>(noutput_items); ++i) {
+            fft1[i] *= std::conj(fft2[i]);
+        }
+
+        // Perform inverse FFT to get the correlation
+        fft(fft1);
+
+        // Find the index of the maximum value
+        double max_norm = std::norm(fft1[0]);
+        size_t index = 0;
+        for (size_t i = 1; i < fft1.size(); ++i) {
+            double curr_norm = std::norm(fft1[i]);
+            if (curr_norm > max_norm) {
+                max_norm = curr_norm;
+                index = i;
+            }
+        }
+
+        if(i%1000 == 0){
+            std::cout << "Index of maximum correlation: " << index << std::endl;
+        }
+        i++;
+        
         output_type* out1 = static_cast<output_type*>(output_items[0]);
         std::copy(in1, in1 + noutput_items, out1);
 
